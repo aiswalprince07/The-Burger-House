546aa559ca2158044d0d9efb143b5d31843fa5a2
Prashant <pkg180901@gmail.com>
2023-03-14 16:28:19 +0530

    google authenticate is done

---
+++ server/routes/user.js
@@ -0,0 +1,19 @@
+import express from "express";
+import passport from "passport";
+
+const router = express.Router();
+
+router.get("/googlelogin",passport.authenticate("google",{
+    scope:["profile"],
+}))
+router.get(
+    "/login",
+    (req,res,next)=>{
+        res.send("Logged In");
+    }
+    // passport.authenticate("google", {
+    // scope:["profile"],
+    //   successRedirect: process.env.FRONTEND_URL,
+    // })
+  );
+export default router; 
---
+++ server/server.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import { connectDB } from "./config/database.js";
+
+connectDB();
+
+app.get("/", (req, res, next) => { 
+    res.send("<h1>Working</h1>");
+  });
+
+app.listen(process.env.PORT,()=>console.log(`Server is working on : ${process.env.PORT}`));  
