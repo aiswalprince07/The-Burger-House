d8fa5ffbebdf1fe119c65c4ceba20592679d3dd9
Prashant <pkg180901@gmail.com>
2023-03-14 16:56:38 +0530

    add gitignore 1

--- server/config/database.js
+++ server/config/database.js
@@ -4,3 +4,4 @@ export const connectDB = async () => {
     const { connection } = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Database is connect with ${connection.host}`);
 }
+   
