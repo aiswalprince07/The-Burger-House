aefbc3359bbb410d7b2d9806ef905ceb4a6e0c4e
aiswalprince07 <81444348+aiswalprince07@users.noreply.github.com>
2023-06-28 20:56:06 +0530

    update backend part

--- server/app.js
+++ server/app.js
@@ -1,44 +1,64 @@
-import  express, { urlencoded }  from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.js";
+import orderRoute from "./routes/Order.js";
 import { connectPassport } from "./utils/Provider.js";
 import session from "express-session";
 import passport from "passport";
-import userRoute from "./routes/user.js"
-import orderRoute from "./routes/order.js"
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import cors from "cors";
 
-const app=express();
+const app = express();
 export default app;
 
 dotenv.config({
-    path:"./config/config.env",
-})  
+  path: "./config/config.env",
+});
 
-//Using Middlewares
+//Using middleware
 app.use(
-    session({
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-})
+
+    cookie: {
+      secure: process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "development" ? false : "none",
+    },
+  })
 );
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(urlencoded({
-    extended:true,
-}))
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(passport.authenticate("session"));
 app.use(passport.initialize());
 app.use(passport.session());
+app.enable("trust proxy");
+
 connectPassport();
 
-// IMporting routes
-app.use("/api/v1",userRoute);
-app.use("/api/v1",orderRoute);
+//Importing Routes
 
+app.use("/api/v1", userRoute);
+app.use("/api/v1", orderRoute);
 
-//using Error Middleware
-app.use(errorMiddleware); 
+//Using middleware
+
+app.use(errorMiddleware);
